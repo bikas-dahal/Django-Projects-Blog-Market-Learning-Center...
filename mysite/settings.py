7391c6f19ce722e3221fe88122cbac9305566ed4
bikky <bikkydahal@gmail.com>
2024-04-21 21:38:11 +0545

    Getting ready for live collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,11 +111,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
