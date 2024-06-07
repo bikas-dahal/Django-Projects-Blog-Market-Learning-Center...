d328a25fe6ee58e1cf7a3a00894195cae16cc72e
bikky <bikkydahal@gmail.com>
2024-06-07 16:29:40 +0545

    Some changes in basic function

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,27 +139,26 @@ ASGI_APPLICATION = 'mysite.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# postgres://django_hcgc_user:0blxQweaQwYgR2BYkgon0XHK8HrQODsp@dpg-cpgg8de3e1ms73affuvg-a.oregon-postgres.render.com/django_hcgc
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'django_hcgc',
-        'USER': 'django_hcgc_user',
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': 'django_hcgc',
+#         'USER': 'django_hcgc_user',
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': '5432',
+#     }
+# }
 
 
 
